d6b5b1b7a5319ad9dce88de151d201d5ff3c3f39
Brian Bartholomew <bartholomewbd@gmail.com>
2019-05-14 12:34:32 -0400

    removed unused imports

--- src/app/components/program-entry/program-entry.component.ts
+++ src/app/components/program-entry/program-entry.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
+import { Component, Input, EventEmitter, Output } from "@angular/core";
 import { Entry } from "src/app/models/entry";
 
 @Component({
--- src/app/components/toolbox/toolbox.component.ts
+++ src/app/components/toolbox/toolbox.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { ProgramEntriesService } from "src/app/services/program-entries.service";
 
 import { Entry } from "../../models/entry";
